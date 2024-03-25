2bdaf7a43fa2843fc72383c2a3e34b480a75a596
Welton Pereira <welton.lps@hotmail.com>
2024-03-25 15:56:14 -0300

    primeiro commit

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+AOS.init();
+
+const dataDoEvento = new Date("jul 27, 2024 20:36:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horaEmMs / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s `;
+    if (distanciaAteOEvento < 0 ){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = "Evento expirado";
+    }
+},1000);
